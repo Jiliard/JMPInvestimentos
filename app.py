ffa5ced9652fb71e90c61f63da71adbf65335bf0
Jiliard <108362526+Jiliard@users.noreply.github.com>
2026-05-14 18:48:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,16 @@ def obter_dados_base():
         return _CACHE["df"].copy()
         
     url = 'https://www.fundamentus.com.br/resultado.php'
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    
+    # DISFARCE AVANÇADO PARA ENGANAR O FIREWALL DO FUNDAMENTUS
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1'
+    }
+    
     r = requests.get(url, headers=headers)
     df = pd.read_html(r.content, thousands='.', decimal=',')[0]
     
